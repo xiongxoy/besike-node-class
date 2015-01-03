13eb043222445e531abbd3c70f0bf5e28b9e0d07
xiongxoy <xiong.xoy@gmail.com>
2015-01-03 22:13:10 +0800

    Implement Methods Inheritance

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 module.exports = Class;
 
-function Class(obj) {
-  var result = obj.initialize ||
-               function () {
-               };
+function Class(child_obj, parent_ctor) {
+  var child_ctor = child_obj.initialize ||
+                   function () {
+                   };
 
-  for (var p in obj) {
-    if (typeof obj[p] == 'function') {
+  for (var p in child_obj) {
+    if (typeof child_obj[p] == 'function') {
       if (p == 'initialize') {
         continue;
       }
-      result.prototype[p] = obj[p];
+      child_ctor.prototype[p] = child_obj[p];
     }
   }
 
-  return result;
+  __extend(child_ctor, parent_ctor);
+  return child_ctor;
 }
+
+function __extend(child_ctor, parent_ctor) {
+  if (parent_ctor) {
+    function ctor() {
+      this.constructor = child_ctor;
+    }
+    ctor.prototype = parent_ctor.prototype;
+    child_ctor.prototype = new ctor;
+  }
+}
+
