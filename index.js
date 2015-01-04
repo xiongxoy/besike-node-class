6573aae2e362ac39101f2dc84d489925db7930ee
xiongxoy <xiong.xoy@gmail.com>
2015-01-04 16:28:54 +0800

    Implement Super

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 module.exports = Class;
 
+
+// Use arguments and Function.prototype.apply to call the super method.
+function __super() {
+  var name = arguments[0];
+  return this.constructor.__super__.prototype[name].apply(this,
+                                                          [].slice.call(arguments,1) );
+}
+
 function Class(child_obj, parent_ctor) {
-  var child_ctor = child_obj.initialize ||
-                   function () {
-                   };
+  var child_ctor =
+    (typeof child_obj.initialize == 'function' && child_obj.initialize) ||
+    (function () { });
+
+  __extend(child_ctor, parent_ctor);
 
   for (var p in child_obj) {
     if (typeof child_obj[p] == 'function') {
@@ -15,19 +25,20 @@ function Class(child_obj, parent_ctor) {
   }
 
   child_ctor.__super__ = parent_ctor || Object;
-  __extend(child_ctor, parent_ctor);
+  child_ctor.prototype.super = __super;
+
 
   return child_ctor;
 }
+
 
 function __extend(child_ctor, parent_ctor) {
   if (parent_ctor) {
     function ctor() {
       this.constructor = child_ctor;
     }
+
     ctor.prototype = parent_ctor.prototype;
     child_ctor.prototype = new ctor;
-
   }
 }
-
